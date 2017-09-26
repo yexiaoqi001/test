c8e387d7f41a980c431bf4f018fdeb40a43b0b27
stpolemo <18311169204@163.com>
2017-09-26 19:29:24 +0800

    完成js

---
+++ js.js
@@ -0,0 +1,7 @@
+var box = document.querySelector('.box');
+box.onclick = function () {
+	alert('This is box!!!!')
+};
+box.style.fontSize = "26px";
+box.style.fontFamily = "楷体";
+box.style.background = "linear gradient(to right, skyblue, pink)";
